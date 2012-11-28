703e87e2e178c77cf7b5d23523f6677e6568698a
Rick Belford <rick@rickbelford.com>
2012-11-28 10:40:48 -0500

    Made map view selected controller, and added info icon

--- DangerZone/DZAppDelegate.m
+++ DangerZone/DZAppDelegate.m
@@ -29,6 +29,11 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
     NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:4 * 1024 * 1024 diskCapacity:20 * 1024 * 1024
                                                              diskPath:nil];
     [NSURLCache setSharedURLCache:URLCache];
+    
+    // make the map view the selected controller
+    UITabBarController * tabBar = (UITabBarController *) self.window.rootViewController;
+    tabBar.selectedViewController = [tabBar.viewControllers objectAtIndex:1];
+    
     /*UITabBarController *tabBarController = (UITabBarController *)[_window rootViewController];
     NSArray *tabs = [tabBarController viewControllers];
     DZTableViewViewController *tableViewController
